fbf52edc6ffdbd584eff1b3e6df50dfd749edd30
Louis Gonzalez <mail@louis.gg>
2021-12-07 18:39:19 -0600

    Work Experience

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -6,6 +6,28 @@ for (i = 0; i < lg_bgItems; i++) {
 	lg_bg.appendChild(lg_bgElement);
 }
 
+// Accordion
+const lg_accordion = document.querySelector('.block__accordion');
+const lg_accordionButtons = lg_accordion.querySelectorAll('.accordion__nav__button');
+if (lg_accordionButtons.length > 0) {
+	lg_accordionButtons.forEach(function (lg_accordionButton) {
+		lg_accordionButton.addEventListener('click', function () {
+			if (!lg_accordionButton.classList.contains('is-active')) {
+				const id = lg_accordionButton.dataset.id;
+				const prev = lg_accordion.querySelector('.accordion__nav__button.is-active');
+
+				// Remove Previous
+				prev.classList.remove('is-active');
+				lg_accordion.querySelector('.accordion__content[data-id="' + prev.dataset.id + '"]').classList.remove('is-active');
+
+				// Update Current
+				lg_accordionButton.classList.add('is-active');
+				lg_accordion.querySelector('.accordion__content[data-id="' + id + '"]').classList.add('is-active');
+			}
+		});
+	});
+}
+
 // Generate Random Number
 function randomNumber(min, max) {
 	return Math.random() * (max - min) + min;
